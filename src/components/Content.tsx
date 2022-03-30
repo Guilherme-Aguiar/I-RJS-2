682e0dd122bbf7c260f70442f918680a6a880cf3
Guilherme de Oliveira Aguiar <gui9627oli@gmail.com>
2022-03-30 20:22:26 -0300

    Resolução do desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,30 @@
-export function Content() {
+import { useState } from 'react';
+
+import { MovieCard } from '../components/MovieCard';
+
+import { Genre, Movie } from '../App'
+
+interface MovieProps {
+  movieList: Movie[],
+  selectedGenre: Genre
+}
+
+export function Content(props: MovieProps) {
   // Complete aqui
+
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.movieList.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
-  // Complete aqui
+import { useState } from 'react';
+
+import { Button } from '../components/Button';
+
+import { Genre } from '../App'
+
+interface GenreResponseProps {
+  genresList: Genre[],
+  onSelectGenre: any
+}
+
+export function SideBar(props: GenreResponseProps) {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  const handleSelectChange = (id: number) => {
+    props.onSelectGenre(id);
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genresList.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleSelectChange(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
